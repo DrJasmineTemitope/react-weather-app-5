05e197436959e3d26787ffa641812ebccd140b2c
DrJasmineTemitope <120563093+DrJasmineTemitope@users.noreply.github.com>
2024-10-27 12:04:22 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
+import SearchEngine from "./SearchEngine";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <button className="btn btn-primary">Shecodes</button>
+        <h1>Weather App</h1>
+        <SearchEngine />
+        <footer>
+          This project is built by{" "}
+          <a
+            href="https://gilded-caramel-cc06eb.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Temitope Lawal {""}
+          </a>
+          and Open Source on Github
+        </footer>
       </div>
     </div>
   );
