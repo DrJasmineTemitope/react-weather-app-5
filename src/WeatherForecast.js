e91a6a549bb67b22ceb8def4499b3065df2f6f71
DrJasmineTemitope <120563093+DrJasmineTemitope@users.noreply.github.com>
2024-11-02 15:23:22 +0000

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,12 +20,15 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.Map(function (dailyForecast, index) {
-            if (index < 5)
+            if (index < 5) {
               return (
                 <div className="col" key={index}>
-                  <ForecastDay data={forecast[0]} />
+                  <ForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
+            }
           })}
         </div>
       </div>
