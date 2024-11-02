b22323417186487243e389f5d119ebabf84fd8c4
DrJasmineTemitope <120563093+DrJasmineTemitope@users.noreply.github.com>
2024-11-02 10:31:11 +0000

    Update the WeatherTemperature JS

--- src/Weather.js
+++ src/Weather.js
@@ -26,13 +26,13 @@ export default function Weather(props) {
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="col-6">
-        <ul>
-          <li>Humidity: {props.data.humidity}%</li>
-          <li>Wind: {props.data.wind} km/h</li>
-        </ul>
+        <div className="col-6">
+          <ul className="WeatherUL">
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
